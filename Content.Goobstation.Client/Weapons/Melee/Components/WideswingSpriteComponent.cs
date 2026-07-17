5497425e19ee9f01254940c444b0c8dad976d786
BeeRobyn <robynthewarcrime@proton.me>
2026-07-17 17:16:26 +0100

    testing changes

---
+++ Content.Goobstation.Client/Weapons/Melee/Components/WideswingSpriteComponent.cs
@@ -0,0 +1,10 @@
+﻿namespace Content.Goobstation.Client.Weapons.Melee.Components;
+
+public sealed class WideswingSpriteComponent
+{
+    [RegisterComponent]
+    public sealed partial class WideswingSpriteComponent : Component
+    {
+
+    }
+}
